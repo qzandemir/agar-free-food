e7b724495dde9618abff8d8d111b6eedaba12217
CuteLifeBot <cutelifebot@gmail.com>
2015-05-16 10:01:51 -0400

    Everything -- all functionality woot woot

---
+++ agar.js
@@ -0,0 +1,64 @@
+// Taken from http://marcgrabanski.com/simulating-mouse-click-events-in-javascript/
+function mouseEvent(type, sx, sy, cx, cy) {
+    var evt;
+    var e = {
+        bubbles: true,
+        cancelable: (type != "mousemove"),
+        view: window,
+        detail: 0,
+        screenX: sx, 
+        screenY: sy,
+        clientX: cx, 
+        clientY: cy,
+        ctrlKey: false,
+        altKey: false,
+        shiftKey: false,
+        metaKey: false,
+        button: 0,
+        relatedTarget: undefined
+    };
+    if (typeof( document.createEvent ) == "function") {
+        evt = document.createEvent("MouseEvents");
+        evt.initMouseEvent(type, 
+            e.bubbles, e.cancelable, e.view, e.detail,
+            e.screenX, e.screenY, e.clientX, e.clientY,
+            e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
+            e.button, document.body.parentNode);
+    } else if (document.createEventObject) {
+        evt = document.createEventObject();
+        for (prop in e) {
+            evt[prop] = e[prop];
+        }
+        evt.button = { 0:1, 1:4, 2:2 }[evt.button] || evt.button;
+    }
+    return evt;
+}
+// End taken section
+
+// Wrap around the above
+function simpleMouseMove(x, y) {
+    return mouseEvent("mousemove", x, y, x, y);
+}
+
+// Restart the game every t milliseconds
+function setNicks(name, t) { setNick(name); if(AGAR.go) { setTimeout(function(){setNicks(name, t)}, t) } } 
+
+//-----------------------------------------------------------------------------
+AGAR = {}
+AGAR.canvas = document.getElementById('canvas')
+AGAR.cx = AGAR.canvas.width/2;
+AGAR.cy = AGAR.canvas.height/2;
+AGAR.go = true;
+AGAR.moveRandomly = function(mint, maxt) {
+    var time = (maxt-mint)*Math.random() + mint;
+    var theta = 2*Math.pi*Math.random();
+    var radius = 50;
+    var x = AGAR.cx + radius*Math.cos(theta);
+    var y = AGAR.cy + radius*Math.sin(theta);
+    if(AGAR.go) {
+        setTimeout(
+            function() { moveRandomly(mint, maxt); },
+            time
+        )
+    }
+}
